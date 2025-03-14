720a46627d089a6a13f716836fb09e24c8624a09
Slimer210 <howweishan@gmail.com>
2025-03-14 14:36:07 +0800

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	let { children } = $props();
+    import "../app.css";
+</script>
+
+<nav class="flex flex-row w-screen text-primary h-24 p-8 items-center shadow-lg border-b-1 border-b-primary/20 fixed top-0 left-0 backdrop-blur-2xl">
+    <a class="flex flex-row h-8 flex-grow gap-4" href="/">
+        <img src="/favicon.png" alt="Logo" />
+        <h2 class="text-2xl font-bold">Law4Minor</h2>
+    </a>
+    <div class="flex flex-row gap-16 uppercase tracking-widest font-light text-primary-text text-base h-fit">
+        <a href="/">Home</a>
+        <a href="/about">About Us</a>
+        <a href="/articles">Articles</a>
+        <a href="/events">Events</a>
+        <a href="/contact">Contact Us</a>
+    </div>
+	
+</nav>
+
+<style lang="postcss">
+</style>
+
+<main class="flex flex-col min-h-screen mt-24 max-w-desktop m-auto">
+    {@render children()}
+</main>
+
+
+
+<footer class="text-primary-text flex flex-row p-10 border-t-1 border-t-primary/20">
+    <p class="flex-grow tracking-wide">&copy; 2024-2025 Law4Minor (P12356). All Rights Reserved.</p>
+    <div class="flex flex-row gap-3 tracking-wider uppercase font-light">
+        <a href="/terms-of-service">Terms of Service</a>
+        <div>|</div>
+        <a href="/privacy-policy">Privacy Policy</a>
+
+    </div>
+    
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+    import Icon from "@iconify/svelte";
+</script>
+
+<section class="p-16 flex flex-row gap-8 items-center">
+    <h1 class="!text-6xl w-1/2 leading-20">What We Care - <br />Addressing The Rise of Juvenile Delinquency</h1>
+    <div class="w-1/2 flex flex-col gap-8">
+        <p class="text-lg">At Law4Minor, we tackle the root causes head-on, taking urgent action through education and awareness—empowering at-risk youth to find support, belonging, and a brighter future.</p>
+        <button class="button-primary">Read More<Icon icon="material-symbols:arrow-right-alt-rounded" width="24" height="24" /></button>
+    </div>
+</section>
+
+<section class="p-16 flex flex-col gap-8 ">
+    <h1>What We Do?</h1>
+    <div class="flex flex-row gap-16">
+        <div class="w-1/2">
+            <img src="./placeholder.jpg" class="rounded-xl h-auto"/>
+        </div>
+        <div class="w-1/2">
+            <p>According to the Department of Statistics Malaysia (DOSM), the number of offences involving minors increased from year to year. At Law4Minor, we aim to target the underlying cause, focusing our urgent action towards education and awareness to help at-risk youth find community.</p>
+            <span class="statistics flex flex-row">
+                <ul>
+                    <li>16</li>
+                    <li>Perrrfrrr</li>
+                </ul>
+                <ul>
+                    <li>16</li>
+                    <li>Perrrfrrr</li>
+                </ul>
+                <ul>
+                    <li>16</li>
+                    <li>Perrrfrrr</li>
+                </ul>
+            </span>
+        </div>
+    </div>
+</section>
