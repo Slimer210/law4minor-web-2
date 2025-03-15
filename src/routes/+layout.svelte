92397fb04251b7b8ea02dc01a47f292b75810de1
Slimer210 <howweishan@gmail.com>
2025-03-15 20:34:09 +0800

    beta code deployment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,12 @@
     import "../app.css";
 </script>
 
-<nav class="flex flex-row w-screen text-primary h-24 p-8 items-center shadow-lg border-b-1 border-b-primary/20 fixed top-0 left-0 backdrop-blur-2xl">
-    <a class="flex flex-row h-8 flex-grow gap-4" href="/">
-        <img src="/favicon.png" alt="Logo" />
-        <h2 class="text-2xl font-bold">Law4Minor</h2>
+<nav class="flex flex-row w-screen text-primary h-18 p-8 items-center shadow-lg border-b-1 border-b-primary/20 fixed top-0 left-0 backdrop-blur-2xl backdrop-brightness-50">
+    <a class="flex flex-row h-8 flex-grow gap-4 items-center" href="/">
+        <img src="/favicon.png" alt="Logo" class="h-8"/>
+        <h2 class="!text-xl !font-semibold">Law4Minor<span class="p-2 mx-4 bg-primary-text/20 border border-primary/50 rounded-sm text-sm font-normal">BETA</span></h2>
     </a>
-    <div class="flex flex-row gap-16 uppercase tracking-widest font-light text-primary-text text-base h-fit">
+    <div class="navlink flex flex-row gap-16 uppercase tracking-widest">
         <a href="/">Home</a>
         <a href="/about">About Us</a>
         <a href="/articles">Articles</a>
@@ -19,6 +19,7 @@
 </nav>
 
 <style lang="postcss">
+
 </style>
 
 <main class="flex flex-col min-h-screen mt-24 max-w-desktop m-auto">
@@ -28,7 +29,7 @@
 
 
 <footer class="text-primary-text flex flex-row p-10 border-t-1 border-t-primary/20">
-    <p class="flex-grow tracking-wide">&copy; 2024-2025 Law4Minor (P12356). All Rights Reserved.</p>
+    <p class="flex-grow tracking-wide text-md">&copy; 2024-2025 Law4Minor (P12356). All Rights Reserved.</p>
     <div class="flex flex-row gap-3 tracking-wider uppercase font-light">
         <a href="/terms-of-service">Terms of Service</a>
         <div>|</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,22 +16,146 @@
         <div class="w-1/2">
             <img src="./placeholder.jpg" class="rounded-xl h-auto"/>
         </div>
-        <div class="w-1/2">
+        <div class="w-1/2 flex flex-col gap-8">
             <p>According to the Department of Statistics Malaysia (DOSM), the number of offences involving minors increased from year to year. At Law4Minor, we aim to target the underlying cause, focusing our urgent action towards education and awareness to help at-risk youth find community.</p>
             <span class="statistics flex flex-row">
                 <ul>
-                    <li>16</li>
-                    <li>Perrrfrrr</li>
+                    <li>10K+</li>
+                    <li>Total Readers of Articles</li>
                 </ul>
+                <span />
                 <ul>
-                    <li>16</li>
-                    <li>Perrrfrrr</li>
+                    <li>100+</li>
+                    <li>Instagram Followers</li>
                 </ul>
+                <span />
                 <ul>
-                    <li>16</li>
-                    <li>Perrrfrrr</li>
+                    <li>200+</li>
+                    <li>Instagram Likes</li>
                 </ul>
             </span>
+            <button class="button-primary">Read Our Story<Icon icon="material-symbols:arrow-right-alt-rounded" width="24" height="24" /></button>
         </div>
     </div>
+</section>
+
+<section class="p-16 flex flex-row gap-16">
+    <div class="w-1/2 flex flex-col gap-8">
+        <h1 class="text-8xl">Our Programs and Resources</h1>
+        <p>At Law4Minor, you can understand more about the law with our quick summaries, long articles, and informative videos.</p>
+        <button class="button-primary">Read our Articles<Icon icon="material-symbols:arrow-right-alt-rounded" width="24" height="24" /></button>
+        <button class="button-accent">Join our Events<Icon icon="material-symbols:arrow-right-alt-rounded" width="24" height="24" /></button>
+    </div>
+    <div class="features w-1/2">
+        <div class="feature-card">
+            <h2><Icon icon="icon-park-solid:online-meeting" width="28" height="28" />Online Engagement</h2>
+            <p>Through engaging and informative legal summaries and witty reels on our Instagram page, you can learn something new about the law.</p>
+        </div>
+        <div class="feature-card">
+            <h2><Icon icon="ooui:article-rtl" width="28" height="28" />Articles</h2>
+            <p>Enjoy 5-minute-reads on the latest news, case studies, and once again, the law to dive deeper. </p>
+        </div>
+        <div class="feature-card">
+            <h2><Icon icon="fluent:people-community-32-filled" width="28" height="28" />Community Outreach</h2>
+            <p>Partnering with other organisations, schools, and firms, we look forward to bringing the fun to you with events and campaigns.</p>
+        </div>
+    </div>
+</section>
+<section class="p-16 flex flex-col gap-8">
+    <h1 class="text-center w-full">Recognition and Partnerships</h1>
+    <p class="font-medium w-3/4 min-w-mobile m-auto">Our initiative aligns with the UN’s Sustainable Development Goal 16:  Peace, Justice, and Strong Institutions. We are actively working with  the Ministry of Education (MOE) and other public sector entities to  ensure that our programs are effective, sustainable, and far-reaching.</p>
+    <div class="m-auto flex flex-row gap-32 !text-primary-text/40">
+        <span>
+            <p class="!text-center">Complies to</p> 
+            <span class="inline-flex gap-2">
+                <Icon icon="fluent:people-community-32-filled" width="72" height="72" />
+                <Icon icon="fluent:people-community-32-filled" width="72" height="72" />
+            </span>
+        </span>
+        <span>
+            <p class="!text-center">Proudly Recognised By</p> 
+            <span class="inline-flex gap-2">
+                <Icon icon="fluent:people-community-32-filled" width="72" height="72" />
+                <Icon icon="fluent:people-community-32-filled" width="72" height="72" />
+                <Icon icon="fluent:people-community-32-filled" width="72" height="72" />
+                <Icon icon="fluent:people-community-32-filled" width="72" height="72" />
+            </span>
+        </span>
+    </div>
+    <button class="button-accent m-auto">Join Us. Make a Difference. <Icon icon="material-symbols:arrow-right-alt-rounded" width="24" height="24" /></button>
+</section>
+
+<section class="p-16 flex flex-col gap-8">
+    <h1 class="">Latest Articles & Posts</h1>
+    <p class="">We are proud of our work, and we are always looking for new challenges. Take a look at some of our recent articles:</p>
+    <button class="button-primary">Read More<Icon icon="material-symbols:arrow-right-alt-rounded" width="24" height="24" /></button>
+    <div class="article-list">
+        <div class="article-card">
+            <img src="./placeholder.jpg" class="rounded-xl h-auto"/>
+            <h3>Online Engagement</h3>
+            <span>
+                <Icon icon="mdi:instagram" width="18" height="18" /> | 
+                <Icon icon="icon-park-solid:calendar" width="18" height="18" /> April 12, 2023
+                <Icon icon="icon-park-solid:people" width="18" height="18" /> CUTE BOI WRITER
+            </span>
+            <p class="text-primary-text font-normal !text-sm">Through engaging and informative legal summaries and witty reels on our Instagram page, you can learn you can lear you can lear you can lear you can lear you can lear...</p>
+            <button class="button-accent">Read More<Icon icon="material-symbols:arrow-right-alt-rounded" width="24" height="24" /></button>
+        </div>
+        <div class="article-card">
+            <img src="./placeholder.jpg" class="rounded-xl h-auto"/>
+            <h3>Online Engagement</h3>
+            <span>
+                <Icon icon="mdi:instagram" width="18" height="18" /> | 
+                <Icon icon="icon-park-solid:calendar" width="18" height="18" /> April 12, 2023
+                <Icon icon="icon-park-solid:people" width="18" height="18" /> CUTE BOI WRITER
+            </span>
+            <p class="text-primary-text font-normal !text-sm">Through engaging and informative legal summaries and witty reels on our Instagram page, you can learn you can lear you can lear you can lear you can lear you can lear...</p>
+            <button class="button-accent">Read More<Icon icon="material-symbols:arrow-right-alt-rounded" width="24" height="24" /></button>
+        </div>
+        <div class="article-card">
+            <img src="./placeholder.jpg" class="rounded-xl h-auto"/>
+            <h3>Online Engagement</h3>
+            <span>
+                <Icon icon="mdi:instagram" width="18" height="18" /> | 
+                <Icon icon="icon-park-solid:calendar" width="18" height="18" /> April 12, 2023
+                <Icon icon="icon-park-solid:people" width="18" height="18" /> CUTE BOI WRITER
+            </span>
+            <p class="text-primary-text font-normal !text-sm">Through engaging and informative legal summaries and witty reels on our Instagram page, you can learn you can lear you can lear you can lear you can lear you can lear...</p>
+            <button class="button-accent">Read More<Icon icon="material-symbols:arrow-right-alt-rounded" width="24" height="24" /></button>
+        </div>
+        <div class="article-card">
+            <img src="./placeholder.jpg" class="rounded-xl h-auto"/>
+            <h3>Online Engagement</h3>
+            <span>
+                <Icon icon="mdi:instagram" width="18" height="18" /> | 
+                <Icon icon="icon-park-solid:calendar" width="18" height="18" /> April 12, 2023
+                <Icon icon="icon-park-solid:people" width="18" height="18" /> CUTE BOI WRITER
+            </span>
+            <p class="text-primary-text font-normal !text-sm">Through engaging and informative legal summaries and witty reels on our Instagram page, you can learn you can lear you can lear you can lear you can lear you can lear...</p>
+            <button class="button-accent">Read More<Icon icon="material-symbols:arrow-right-alt-rounded" width="24" height="24" /></button>
+        </div>
+        <div class="article-card">
+            <img src="./placeholder.jpg" class="rounded-xl h-auto"/>
+            <h3>Online Engagement</h3>
+            <span>
+                <Icon icon="mdi:instagram" width="18" height="18" /> | 
+                <Icon icon="icon-park-solid:calendar" width="18" height="18" /> April 12, 2023
+                <Icon icon="icon-park-solid:people" width="18" height="18" /> CUTE BOI WRITER
+            </span>
+            <p class="text-primary-text font-normal !text-sm">Through engaging and informative legal summaries and witty reels on our Instagram page, you can learn you can lear you can lear you can lear you can lear you can lear...</p>
+            <button class="button-accent">Read More<Icon icon="material-symbols:arrow-right-alt-rounded" width="24" height="24" /></button>
+        </div>
+        <div class="article-card">
+            <img src="./placeholder.jpg" class="rounded-xl h-auto"/>
+            <h3>Online Engagement</h3>
+            <span>
+                <Icon icon="mdi:instagram" width="18" height="18" /> | 
+                <Icon icon="icon-park-solid:calendar" width="18" height="18" /> April 12, 2023
+                <Icon icon="icon-park-solid:people" width="18" height="18" /> CUTE BOI WRITER
+            </span>
+            <p class="text-primary-text font-normal !text-sm">Through engaging and informative legal summaries and witty reels on our Instagram page, you can learn you can lear you can lear you can lear you can lear you can lear...</p>
+            <button class="button-accent">Read More<Icon icon="material-symbols:arrow-right-alt-rounded" width="24" height="24" /></button>
+        </div>
+    </div>
+
 </section>
