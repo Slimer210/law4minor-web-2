c45933d3bacfade8acf05a60280ef8a577f1e116
侯韦杉 <66945259+Slimer210@users.noreply.github.com>
2025-07-11 08:04:45 +0000

    temporary update

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Button from '$lib/components/element/button.svelte';
+    import Icon from '@iconify/svelte';
+</script>
+
+<section class="p-8 mobile:p-16 flex flex-col tablet:flex-row gap-16 bg-gray-500">
+    <div class="grid w-full h-full grid-cols-3 grid-rows-2">
+        <div class="col-span-3 row-span-1">
+            <h1 class="text-3xl font-bold text-white">Contact Us</h1>
+            <p class="mt-4 text-lg text-gray-200">We would love to hear from you! Please fill out the form below or reach out via email.</p>
+
+        </div>
+    </div>
+</section>
